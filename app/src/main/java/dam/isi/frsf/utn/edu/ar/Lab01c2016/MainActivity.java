a5b4f3de829f607d75a86b2eab4c7ff3acc7aa7d
Adrielg <ag.castelucci@gmail.com>
2016-09-07 19:46:32 -0300

    mensajito con errores

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/Lab01c2016/MainActivity.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/Lab01c2016/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText importe, dias;
     private TextView resultado;
     private Button boton;
+    private SeekBar dias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
